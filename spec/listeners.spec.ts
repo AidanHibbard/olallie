328d3db10b25b79fbc9f79d33cf4ecc437319e15
Aidan Hibbard <aidanchibbard@gmail.com>
2023-12-24 16:28:43 -0800

    Update listeners usage (#3)
    * bump deps, lint, clean readme
    
    * update listeners usage
    
    * use sets instead of arrays for listeners

--- spec/listeners.spec.ts
+++ spec/listeners.spec.ts
@@ -2,6 +2,12 @@ import { expect, it, describe } from 'vitest';
 import listeners_store from './mock_stores/listeners_store';
 
 describe('Store Listeners', () => {
+  const listener_one = listeners_store.listen('count', (value) => {
+    listeners_store.event_count = value;
+  });
+  const listener_two = listeners_store.listen('count', (value) => {
+    listeners_store.second_event_count = value;
+  });
   describe('Initial State', () => {
     it('Should have default values', () => {
       expect(listeners_store.event_count).toEqual(0);
@@ -9,18 +15,29 @@ describe('Store Listeners', () => {
       expect(listeners_store.count).toEqual(0);
     });
   });
-  describe('Listeners', () => {
+  describe('Listen', () => {
     it('Should listen for state changes', () => {
-      listeners_store.listen('count', (value) => {
-        listeners_store.event_count = value;
-      });
       listeners_store.count++;
+      expect(listeners_store.count).toEqual(1);
       expect(listeners_store.event_count).toEqual(listeners_store.count);
+      expect(listeners_store.second_event_count).toEqual(listeners_store.count);
     });
-    it('Should remove listeners for state item', () => {
-      listeners_store.unlisten('count');
+  });
+  describe('Unlisten', () => {
+    it('Should remove listeners for state item and return true', () => {
+      expect(listener_one.unlisten()).toBeTruthy();
       listeners_store.count++;
+      expect(listeners_store.count).toEqual(2);
       expect(listeners_store.event_count).toEqual(listeners_store.count - 1);
+    });
+    it('Should not remove other listeners for the same key', () => {
+      expect(listeners_store.second_event_count).toEqual(listeners_store.count);
+    });
+    it('Should return false if listener was already removed', () => {
+      expect(listener_one.unlisten()).toBeFalsy();
+    });
+    it('Should return true for second listener unlistening on the same key', () => {
+      expect(listener_two.unlisten()).toBeTruthy();
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ export function createStore<
   G,
 >(options: StoreOptions<S, A, G>): Store<S, A, G> {
   const stateProxy = createProxy(options.state(), (key, value) => {
-    triggerListeners(key as keyof S, value);
+    triggerListeners(key, value);
   });
 
-  const listeners: Record<string, Array<(value: any) => void>> = {};
+  const listeners: Record<string, Set<(value: any) => void>> = {};
 
   for (const key in options.actions) {
-    if (Object.hasOwnProperty.call(options.actions, key)) {
+    if (Object.hasOwn(options.actions, key)) {
       const action = options.actions[key];
       (stateProxy as Record<string, (...args: any[]) => any>)[key] = (
         ...args: any[]
@@ -25,7 +25,7 @@ export function createStore<
   }
 
   for (const key in options.getters) {
-    if (Object.hasOwnProperty.call(options.getters, key)) {
+    if (Object.hasOwn(options.getters, key)) {
       const getter = options.getters[key];
       Object.defineProperty(stateProxy, key, {
         get: () => getter(stateProxy),
@@ -39,21 +39,25 @@ export function createStore<
     callback: (value: S[K]) => void,
   ) {
     if (!listeners[key as string]) {
-      listeners[key as string] = [];
+      listeners[key as string] = new Set();
     }
-    listeners[key as string].push(callback);
-  };
+    listeners[key as string].add(callback);
 
-  (stateProxy as Store<S, A, G>).unlisten = function <K extends keyof S>(
-    key: K,
-  ) {
-    delete listeners[key as string];
+    return {
+      unlisten: () => {
+        const listenerSet = listeners[key as string];
+        if (listenerSet) {
+          return listenerSet.delete(callback);
+        }
+        return false;
+      },
+    };
   };
 
   function triggerListeners<K extends keyof S>(key: K, value: S[K]) {
-    const keyListeners = listeners[key as string];
-    if (keyListeners) {
-      for (const callback of keyListeners) {
+    const listenerSet = listeners[key as string];
+    if (listenerSet) {
+      for (const callback of listenerSet) {
         callback(value);
       }
     }
